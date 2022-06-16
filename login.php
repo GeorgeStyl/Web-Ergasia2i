8d26aa6f7bfff2ad00f777f4f5d39918ddb46871
geosty <dit19219@go.uop.gr>
2022-06-16 11:47:30 -0700

    finished login-signup need to fix  UI

--- login.php
+++ login.php
@@ -1,7 +1,13 @@
 <?php
-    session_destroy();
+
+if(session_id() != '' || isset($_SESSION) || session_status() === PHP_SESSION_ACTIVE) {
+    // start sess
+   session_destroy();
+  }
+
 
     if(isset($_SERVER['HTTPS'])) $https = $_SERVER['HTTPS'];
+    else $https="";
     $serverport = $_SERVER['SERVER_PORT']; 
     $servname = $_SERVER['SERVER_NAME'];  
     // If the script is running on a virtual host, this will be the value defined for that virtual host.
@@ -19,63 +25,80 @@
     $mysqluser = "webapp";
     $mysqlpwd = "123456";
 
+
+    $haserror = false;
+    $errormessage="";
+
     // if called with other verb than post we dont accept the request
     if( $_SERVER['REQUEST_METHOD'] != 'POST') {
         // build the sign-up.html url
-        $url = "$baseurl/sign-up.html";
+        $url = "$baseurl/login.html";
         // redirect back to sign-up.html
-        header("Location: $url");
+        header("Location: $url?loginerror=Bad_Method_Used");
         // exit without any other for not to break the redirection
         die();
     }
 
-
     // form url encoded data post =  usr=....&psw=....
-    $fusr = $_POST["usr"];
-    $fpsw = $_POST["psw"];
-
+    if(!isset($_POST["usr"])){
+        $url = "$baseurl/login.html";
+        header("Location: $url?loginerror=No_Username");
+        die();
+    }
+    $usr = $_POST["usr"];
+    if(!isset($_POST["psw"])){
+        $url = "$baseurl/login.html";
+        header("Location: $url?loginerror=No_Password");
+        die();
+    }
+    $psw = $_POST["psw"];
     
     // Create connection
     $conn = new mysqli($mysqlserver, $mysqluser, $mysqlpwd, $mysqldb);
 
     // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        $url = "$baseurl/login.html";
+        header("Location: $url?loginerror=Server_Error");
+        die();
     }
-
-    
-
-
 
     $sql ="select count(*) as ISVALID from `users` where ( `password`='$psw' or `username`='$usr') ;";
 
-    $validregistration = true;
+    $validregistration = false;
 
     $result = $conn->query($sql);
     if ($result->num_rows == 1) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
         // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-        if( $row["ISVALID"] > 0 ) $validregistration=false;
-    }
-    }else{
-    // this shouldnt happen
-    echo "ERROR:: 2 or more users with the same credentials found" ;
+            if( $row["ISVALID"] > 0 ) $validregistration=true;
+            break;
+        }
+    } else {
+        // this shouldnt happen
+        $url = "$baseurl/login.html";
+        header("Location: $url?loginerror=Dublicate_User");
+        die();
     }
 
 
     if($validregistration==false)
     {
-        echo "        
-            <script type=\"text/javascript\"> 
-                alert(\"Λανθασμένο όνομα χρήστη ή κωδικός\");
-            </script>";
-
-        $url = "$baseurl/sign-up.html?error=username_or_email_exists";
+        $url = "$baseurl/login.html?loginerror=Bad_Username_or_Email";
         // redirect back to sign-up.html
+        // header("GS-Error: Login Error, Bad Username or Email");
         header("Location: $url");
         // exit without any other for not to break the redirection
         die();
     }
+
+    // login is success goto basics.html
+    $url = "$baseurl/basics.html?login=success";
+    // redirect back to sign-up.html
+    header("Location: $url");
+    // exit without any other for not to break the redirection
+    die();
+
 
 ?>
--- sign-up.php
+++ sign-up.php
@@ -1,135 +1,135 @@
 
 <?php
 
-  // if(session_id() != '' || isset($_SESSION) || session_status() === PHP_SESSION_ACTIVE) {
-  //  // session isn't started
-  //  session_destroy();
-  // }
-  session_destroy();
-
-  if(isset($_SERVER['HTTPS'])) $https = $_SERVER['HTTPS'];
-  $serverport = $_SERVER['SERVER_PORT']; 
-  $servname = $_SERVER['SERVER_NAME'];  
-    // If the script is running on a virtual host, this will be the value defined for that virtual host.
-  $servaddr = $_SERVER['SERVER_ADDR']; 
-  if($https == "") $https = "http://" ;  // alloiws https://
-  $baseurl = $https . $servname . ":" . $serverport ; // build the base url, example http://localhost:80
-  $self    = $_SERVER['PHP_SELF'] ;        // in a php script at the address http://example.com/foo/bar.php would be /foo/bar.php
-  $webpath = dirname($_SERVER['PHP_SELF']); // in a php script at the address http://example.com/foo/bar.php would be /foo
-  $fullwebpath = $baseurl . $webpath ;      // for this page would be http://localhost:80/webergasia1
-
-
-
-    // if called with other verb than post we dont accept the request
-    if( $_SERVER['REQUEST_METHOD'] != 'POST') {
-        // build the sign-up.html url
-        $url = "$baseurl/sign-up.html";
-        // redirect back to sign-up.html
-        header("Location: $url");
-        // exit without any other for not to break the redirection
-        die();
+  if(session_id() != '' || isset($_SESSION) || session_status() === PHP_SESSION_ACTIVE) {
+      // start sess
+    session_destroy();
     }
 
-    // it is posted ...
+    if(isset($_SERVER['HTTPS'])) $https = $_SERVER['HTTPS'];
+    else $https="";
+    $serverport = $_SERVER['SERVER_PORT']; 
+    $servname = $_SERVER['SERVER_NAME'];  
+      // If the script is running on a virtual host, this will be the value defined for that virtual host.
+    $servaddr = $_SERVER['SERVER_ADDR']; 
+    if($https == "") $https = "http://" ;  // alloiws https://
+    $baseurl = $https . $servname . ":" . $serverport ; // build the base url, example http://localhost:80
+    $self    = $_SERVER['PHP_SELF'] ;        // in a php script at the address http://example.com/foo/bar.php would be /foo/bar.php
+    $webpath = dirname($_SERVER['PHP_SELF']); // in a php script at the address http://example.com/foo/bar.php would be /foo
+    $fullwebpath = $baseurl . $webpath ;      // for this page would be http://localhost:80/webergasia1
 
 
 
-
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $Day = $_POST['Day'];
-    $month = $_POST['month'];
-    $year = $_POST['year'];
-    $Gender = $_POST['Gender'];
-    $usr = $_POST['usr'];
-    $email = $_POST['email'];
-    $psw = $_POST['psw'];
-    $remember = "";
-    if(isset($_POST['remember'])) $remember = $_POST['remember'];
-    $DOB = "$year$month$Day";
-
-    // login to DataBase credentials
-    $mysqlserver = "localhost";
-    $mysqldb = "teliki_ergasia";
-    $mysqluser = "webapp";
-    $mysqlpwd = "123456";
-
-
-    // Create connection
-    $conn = new mysqli($mysqlserver, $mysqluser, $mysqlpwd, $mysqldb);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-
-    $sql="select count(*) as CNTUSRS from `users` where ( `email`='$email' or `username`='$usr') ;";
-
-    $validregistration = true;
-
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-        if( $row["CNTUSRS"] > 0 ) $validregistration=false;
+      // if called with other verb than post we dont accept the request
+      if( $_SERVER['REQUEST_METHOD'] != 'POST') {
+          // build the sign-up.html url
+          $url = "$baseurl/sign-up.html";
+          // redirect back to sign-up.html
+          header("Location: $url");
+          // exit without any other for not to break the redirection
+          die();
       }
-    } else {
-    //  
-    }
 
-    if($validregistration==false)
-    {
-        $url = "$baseurl/sign-up.html?error=username_or_email_exists";
-        // redirect back to sign-up.html
-        header("Location: $url");
-        // exit without any other for not to break the redirection
-        die();
-    }
+      // it is posted ...
 
 
 
 
-  //    $sql = "INSERT INTO `users`(`FirstName`, `LastName`, `DateOfBirth`, `Gender`, `username`, `password`, `email`, `DateSigned`, `DateUpdated`, `IsActive` ,`sessionID`, `sessionDT`) VALUES (". 
-  //    " '$fname', '$lname', '$DOB', '$Gender', '$usr', '$psw', '$email' , now() , now() , 1, '" . SID . "', now() ) ;";
-  // sessionID 
-  // sessionDT
+      $fname = $_POST['fname'];
+      $lname = $_POST['lname'];
+      $Day = $_POST['Day'];
+      $month = $_POST['month'];
+      $year = $_POST['year'];
+      $Gender = $_POST['Gender'];
+      $usr = $_POST['usr'];
+      $email = $_POST['email'];
+      $psw = $_POST['psw'];
+      $remember = "";
+      if(isset($_POST['remember'])) $remember = $_POST['remember'];
+      $DOB = "$year$month$Day";
 
-  $sql = "INSERT INTO `users`(`FirstName`, `LastName`, `DateOfBirth`, `Gender`, `username`, `password`, `email`, `DateSigned`, `DateUpdated`, `IsActive` ) VALUES (". 
-  " '$fname', '$lname', '$DOB', '$Gender', '$usr', '$psw', '$email' , now() , now() , 1 ) ;";
+      // login to DataBase credentials
+      $mysqlserver = "localhost";
+      $mysqldb = "teliki_ergasia";
+      $mysqluser = "webapp";
+      $mysqlpwd = "123456";
 
-  if ($conn->query($sql) === TRUE) {
 
-        // TODO:: if remember me == yes , loginn in backgorund here, redirect to to quiz
-        if($remember=="on") {
-          session_start();    // global $_SESSION created here  
-          $_SESSION["username"] = $usr;     
-          $_SESSION["firstname"]= $fname;     
-          $_SESSION["lastname"] = $lname;    
-          $_SESSION["email"]    = $email;    
-          $url = "$baseurl/quiz.php";
+      // Create connection
+      $conn = new mysqli($mysqlserver, $mysqluser, $mysqlpwd, $mysqldb);
+
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+
+
+      $sql="select count(*) as CNTUSRS from `users` where ( `email`='$email' or `username`='$usr') ;";
+
+      $validregistration = true;
+
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+          if( $row["CNTUSRS"] > 0 ) $validregistration=false;
         }
-        else {
-          // else if not remember me then redirect to login
-          $url = "$baseurl/login.html";
-        }
-        // redirect back to sign-up.html
-        header("Location: $url");
-        // exit without any other for not to break the redirection
-        die();
+      } else {
+      //  
+      }
 
-    } else {
-        $url = "$baseurl/sign-up.html?error=registration_failed";
-        // redirect back to sign-up.html
-        header("Location: $url");
-        // exit without any other for not to break the redirection
-        die();
-    }
+      if($validregistration==false)
+      {
+          $url = "$baseurl/sign-up.html?error=username_or_email_exists";
+          // redirect back to sign-up.html
+          header("Location: $url");
+          // exit without any other for not to break the redirection
+          die();
+      }
 
-  $conn->close();
 
-  //    echo $sql;
+
+
+    //    $sql = "INSERT INTO `users`(`FirstName`, `LastName`, `DateOfBirth`, `Gender`, `username`, `password`, `email`, `DateSigned`, `DateUpdated`, `IsActive` ,`sessionID`, `sessionDT`) VALUES (". 
+    //    " '$fname', '$lname', '$DOB', '$Gender', '$usr', '$psw', '$email' , now() , now() , 1, '" . SID . "', now() ) ;";
+    // sessionID 
+    // sessionDT
+
+    $sql = "INSERT INTO `users`(`FirstName`, `LastName`, `DateOfBirth`, `Gender`, `username`, `password`, `email`, `DateSigned`, `DateUpdated`, `IsActive` ) VALUES (". 
+    " '$fname', '$lname', '$DOB', '$Gender', '$usr', '$psw', '$email' , now() , now() , 1 ) ;";
+
+    if ($conn->query($sql) === TRUE) {
+
+          // TODO:: if remember me == yes , loginn in backgorund here, redirect to to quiz
+          if($remember=="on") {
+            session_start();    // global $_SESSION created here  
+            $_SESSION["username"] = $usr;     
+            $_SESSION["firstname"]= $fname;     
+            $_SESSION["lastname"] = $lname;    
+            $_SESSION["email"]    = $email;    
+            $url = "$baseurl/quiz.php";
+          }
+          else {
+            // else if not remember me then redirect to login
+            $url = "$baseurl/login.html";
+          }
+          // redirect back to sign-up.html
+          header("Location: $url");
+          // exit without any other for not to break the redirection
+          die();
+
+      } else {
+          $url = "$baseurl/sign-up.html?error=registration_failed";
+          // redirect back to sign-up.html
+          header("Location: $url");
+          // exit without any other for not to break the redirection
+          die();
+      }
+
+    $conn->close();
+
+    //    echo $sql;
 
 
 
